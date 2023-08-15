7a2140d23ce7835777c02166eb0a4a9f5bcb2173
geolffrey <geolffrey@gmail.com>
2023-08-15 21:02:30 +0000

    Deploying to gh-pages from  @ fc245c1b2e99a748e8f6cd284ba9c50471911da3 🚀

---
+++ Pages/CirclePacking.razor.js
@@ -0,0 +1,49 @@
+﻿import { CirclePack } from '../js/CirclePack.js'
+
+let circlePacking = {};
+let callerObjectRef = {};
+
+export function browserDimmensions() {
+    return { width: window.innerWidth, height: window.innerHeight };
+}
+
+export function init(objectRef) {
+    callerObjectRef = objectRef;
+
+    import('../lib/d3.v7.min.js').then((/*d3*/) => {
+        circlePacking = new CirclePack();
+        circlePacking.width = window.innerWidth;
+        circlePacking.height = window.innerHeight;
+        circlePacking.setCallBack = (element) => {
+            callerObjectRef.invokeMethodAsync('OnSelectedPoint', element);
+        };
+        
+        circlePacking.init();
+    });
+};
+
+
+
+export function onclickZoomIn() {
+    circlePacking.zoomIn();
+};
+
+export function onclickZoomOut() {
+    circlePacking.zoomOut();
+};
+
+export function onclickzoomRandom() {
+    circlePacking.zoomRandom();
+};
+
+
+export function onclickzoomReset() {
+    circlePacking.zoomReset();
+}
+export function addDataSet(ds) {
+    circlePacking.addDataSet(ds)
+}
+export function dispose() {
+    circlePacking.dispose();
+    circlePacking = null;
+}
